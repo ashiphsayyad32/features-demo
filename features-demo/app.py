f9f3bd34dcd833809b128fbcce34e67028dc8401
Ashiph <ashiphs@cybage.com>
2025-07-04 16:14:54 +0530

    Add intentional KeyError bug route for bug bot demo

--- features-demo/app.py
+++ features-demo/app.py
@@ -11,5 +11,10 @@ def bug():
     # Intentional bug for demo
     return str(1 / 0)
 
+@app.route('/keyerror')
+def keyerror():
+    d = {'a': 1}
+    return str(d['b'])  # Intentional KeyError
+
 if __name__ == '__main__':
     app.run(debug=True) 
